964bd3493e3d0fb3d352a4007bf597114e53baef
Jorge Nicolau <4923759+jlnicolau@users.noreply.github.com>
2023-05-15 21:30:43 -0700

    included arguments in launch.json for running in debug mode

--- fileNameChanger.py
+++ fileNameChanger.py
@@ -1,2 +1,23 @@
+import sys
+import os
+import re
+from pathlib import Path
 
+#check if the folder already have a log. If yes, it will close the program to avoid to lose the previous log file.
+if os.path.isfile("logFileNameChanges.txt"):
+   sys.exit("The program will not execute because there is a logFile in the folder.")
+
+#check if the path is valid
+print(sys.argv[1])
+pathRoot = Path(sys.argv[1])
+
+
+
+for path in pathRoot.iterdir():
+    print(path)
+   
+
+
+# if not os.path.isdir(photoRoot):
+#     sys.exit("The given path doesn't exist.")
 
